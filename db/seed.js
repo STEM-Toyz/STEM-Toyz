085fbda518efc517776af4a1b23025e72927c673
Ming Tung Wong <mingtungwong@outlook.com>
2017-01-10 15:33:56 -0500

    Small additions to seed file

--- db/seed.js
+++ db/seed.js
@@ -5,9 +5,19 @@ const seedUsers = () => db.Promise.map([
   {firstName: 'Barack', lastName: 'Obama', email: 'bamad@example.com', phoneNumber:"1223425678", password: '123434'},
 ], user => db.model('users').create(user))
 
+const seedAddresses = () => db.Promise.map([
+  {street: '123 Fake St', city: 'New York', state: 'NY', country: 'USA', zip: '10036', user_id: 1},
+  {street: '5 Hanover Sq', city: 'New York', state: 'NY', country: 'USA', zip: '10004', user_id: 1},
+  {street: '2 Hanover Sq', city: 'New York', state: 'NY', country: 'USA', zip: '10004', user_id: 2}
+], address => db.model('address').create(address));
+
+
+
 db.didSync
   .then(() => db.sync({force: true}))
   .then(seedUsers)
   .then(users => console.log(`Seeded ${users.length} users OK`))
+  .then(seedAddresses)
+  .then(addresses => console.log(`Seeded ${addresses.length} addresses OK`))
   .catch(error => console.error(error))
   .finally(() => db.close())
