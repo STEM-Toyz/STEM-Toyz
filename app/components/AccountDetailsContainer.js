d333e4b7c3d050fde5f30912647398e76af18503
Ming Tung Wong <mingtungwong@outlook.com>
2017-01-12 17:49:11 -0500

    Create initial component and container files including boilerplate

---
+++ app/components/AccountDetailsContainer.js
@@ -0,0 +1,12 @@
+import AccountDetails from './AccountDetails';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+  return {};
+}
+
+const mapDispatchToProps = dispatch => {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountDetails);
