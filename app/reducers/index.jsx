143900e6fc21f88ee348d69522fe38679d879841
RTongue <r.tongue@gmail.com>
2017-01-12 12:56:53 -0500

    Added Redux DevTools to Store.jsx

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 
-const rootReducer = combineReducers({
-  auth: require('./auth').default,  
+const rootReducer = ({
+  auth: require('./auth').default,
 })
 
 export default rootReducer
