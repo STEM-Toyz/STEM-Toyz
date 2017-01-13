de81e8d10de6fba6543462067cbdad2b04eb3beb
Sean Martin <srmartin777@gmail.com>
2017-01-13 15:57:09 -0500

    started work on products commonent

---
+++ app/components/Products.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react';
+import { Link } from 'react-router';
+
+export default function (props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ app/containers/ProductsContainer.js
@@ -0,0 +1,13 @@
+'use strict';
+import { connect } from 'react-redux';
+import Products from 'APP/app/components/Products';
+
+const mapStateToProps = (state, ownProps) => {
+  return {};
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
