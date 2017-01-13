8323ab4ca09b0917dc9d18395dedd148c9e11b72
RTongue <r.tongue@gmail.com>
2017-01-13 13:51:45 -0500

    Basic Navbar Tests implemented

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -12,19 +12,20 @@ export default (props) => {
   const user = props.user;
 
   return (
-  <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-    <img src="NoLogo.png" className="logo" />
-    <div id="search">
-      <form className="form-inline" onSubmit={handleSubmit}>
-        <input className="form-control mr-sm-2" type="text" placeholder="Search" onChange={handleChange}></input>
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-    <div id="login">
-      {user ? <WhoAmI /> : <Login />}
-    </div>
-    <div id="cart">
-      <button className="btn btn-outline-success my-2 my-sm-0 pull-right" type="submit">Shopping Cart</button>
-    </div>
-  </nav>
-)};
+    <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+      <img src="NoLogo.png" className="logo" />
+      <div id="search">
+        <form className="form-inline" onSubmit={handleSubmit}>
+          <input className="form-control mr-sm-2" type="text" placeholder="Search" onChange={handleChange}></input>
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+      <div id="login">
+        {user ? <WhoAmI /> : <Login />}
+      </div>
+      <div id="cart">
+        <button className="btn btn-outline-success my-2 my-sm-0 pull-right" type="submit">Shopping Cart</button>
+      </div>
+    </nav>
+  );
+};
