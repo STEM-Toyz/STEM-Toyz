9cad5906af26eefff83de2b692308166bd7fad47
Ming Tung Wong <mingtungwong@outlook.com>
2017-01-14 15:20:06 -0500

    Make initial files

---
+++ app/containers/OrdersContainer.jsx
@@ -0,0 +1,12 @@
+import Orders from '../components/Orders';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+  return {};
+}
+
+const mapDispatchToProps = dispatch => {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);
---
+++ app/reducers/orders.jsx
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const inititalState = {
+  orders: []
+}
+
+const SELECT_ORDERS = 'SELECT_ORDERS';
+
+export const fetchOrders = userId => {
+  return function(dispatch) {
+    
+  }
+}
