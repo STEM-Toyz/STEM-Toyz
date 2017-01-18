516133e77aac86b4b3ef489bf35fee39fe5284dc
Ming Tung Wong <mingtungwong@outlook.com>
2017-01-17 20:03:03 -0500

    Fix Logout Button

--- app/components/WhoAmI.jsx
+++ app/components/WhoAmI.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router';
 
 export const WhoAmI = ({ user, logout }) => (
   <div className="whoami">
-    <div className="vertical">
-      <span id="user" className="glyphicon glyphicon-user"></span>
-      <p className="whoami-user-name">Hi {user && user.firstName}!</p>
-    </div>
+     <Link to={`/account/${user.id}`}>
+      <div className="vertical">
+       <span id="user" className="glyphicon glyphicon-user"></span>
+        <p className="whoami-user-name">Hi {user && user.firstName}!</p>
+      </div>
+    </Link>
     <button className="logout btn btn-primary btn-outline-success my-2 my-sm-0" onClick={logout}>Logout</button>
   </div>
 )
